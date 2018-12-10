cf995791aa2808d397d4cae7f15940c0b8bac488
Claudiu Stanciu <claudiu.stanciu@bglgroup.co.uk>
2018-12-10 17:04:10 +0000

    Add time based action

--- infra_maid.py
+++ infra_maid.py
@@ -1,4 +1,6 @@
+import datetime
 import json
+import os
 import re
 
 import boto3
@@ -37,26 +39,65 @@ def is_matching_instance(ec2_instance, patterns):
 
 def save_local(filename, data):
     with open(filename, 'w') as f:
-        f.write(data)
+        json.dump(data, f, sort_keys=True, indent=2)
 
 
-def main():
-    whitelist = ['^data']
-    blacklist = ['.*cdm.*', '.*shadow.*']
-    ec2 = boto3.resource('ec2')
-    running_instances = get_instances(ec2, )
+def load_local(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
 
-    # whitelisted_instances = filter(lambda i: is_matching_instance(i, whitelist), ec2_instances)
-    matching_instances = filter(lambda i: is_matching_instance(i, whitelist) and not is_matching_instance(i, blacklist),
-                                running_instances)
 
-    name_ids = []
+def stop_instances(ec2):
+    running_instances = get_instances(ec2)
+
+    stop_patterns = ['^data']
+    ignore_patterns = ['.*cdm.*', '.*shadow.*']
+
+    matching_instances = filter(
+        lambda i: is_matching_instance(i, stop_patterns) and not is_matching_instance(i, ignore_patterns),
+        running_instances
+    )
+
+    running_instances = []
     for instance in matching_instances:
         for tag in instance.tags:
             if tag['Key'] == 'Name':
-                name_ids.append({'id': instance.id, 'name': tag['Value']})
+                running_instances.append({'id': instance.id, 'name': tag['Value']})
 
-    save_local("running_ids.json", json.dumps(name_ids, sort_keys=True, indent=2))
+    filename = 'instances.json'
+    save_local(filename, running_instances)
+
+    print('Shutdown instances. Exiting ...')
+
+
+def start_instances():
+    filename = 'instances.json'
+    if os.path.isfile(filename):
+        stopped_instances = load_local(filename)
+        for instance in stopped_instances:
+            print(instance['id'])
+        print('Started instances')
+
+    else:
+        print('No instances.json file found. No instance started. Exiting ...')
+
+
+def check_action(ec2):
+    current_time = datetime.datetime.now().time()
+    # current_time = datetime.time(10)
+    start_time = datetime.time(8, 0)
+    shutdown_time = datetime.time(20, 0)
+
+    if start_time <= current_time < shutdown_time:
+        start_instances()
+    else:
+        stop_instances(ec2, )
+
+
+def main():
+    ec2 = boto3.resource('ec2')
+    check_action(ec2)
 
 
 if __name__ == '__main__':
